5b992f5f4344c11b1b42700cd000fff6be7d29e9
hitemth <ivashkina.em@phystech.edu>
2023-06-08 01:10:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,20 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 156287560 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(control_npv, test_npv) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    from scipy.stats import ttest_ind
+
+    # Рассчитываем среднее значение NPV в контрольной группе
+    control_mean = sum(control_npv) / len(control_npv)
+    
+    # Рассчитываем среднее значение NPV в тестовой группе
+    test_mean = sum(test_npv) / len(test_npv)
+    
+    # Выполняем t-тест Стьюдента для независимых выборок
+    t_stat, p_value = ttest_ind(control_npv, test_npv, equal_var=False)
+    
+    # Определяем, отклоняем ли нулевую гипотезу
+
+    return p_value < 0.03
